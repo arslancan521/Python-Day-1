53333a907e58a29df43690c0001d8902d61e82b0
CAN ARSLAN <96886391+arslancan521@users.noreply.github.com>
2022-08-04 17:36:58 +0300

    Add files via upload

---
+++ Day 1 - Basic Input.py
@@ -0,0 +1,10 @@
+
+print("**********************************************")
+
+name = str(input("Please enter your name : "))
+
+company = str(input("Please enter your company name : "))
+
+print(f"{name} is working for {company}")
+
+
